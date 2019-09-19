449b9a1cf6a123e5fa9e55040916276428bf2f55
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-18 21:23:19 -0500

    GIPHY search issues

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,6 +25,26 @@ $("#add-animal").on("click", function(event) {
 $("button").on("click", function() {
     var animal = $(this).attr("data-animal");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=q6mqgFplDJRCDJsdFupOB05a6VWXakpM&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r") {
+                    var animalDiv = $("<div>");
+                    var animalImg = $("<img>");
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    animalImg.attr("src", results[i].images.fixed_height.url);
+                    animalDiv.append(p);
+                    animalDiv.append(animalImg);
+                    $("#images").append(animalDiv);
+                }
+            }
+        })
 });
 
 renderButtons();
